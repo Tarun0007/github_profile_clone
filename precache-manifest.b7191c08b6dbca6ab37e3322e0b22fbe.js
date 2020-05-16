2bed7840774d3215f21dce1e38af2b79b9cce412
Tarun Thakur <tarun@tripeur.com>
2020-05-16 10:19:31 +0530

    Updates

---
+++ precache-manifest.b7191c08b6dbca6ab37e3322e0b22fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cab590bf933adeef3b1dc6faf887bb6",
+    "url": "/my-reactapp/index.html"
+  },
+  {
+    "revision": "9255f6eb03de8dcf9069",
+    "url": "/my-reactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae7ffb4d89426179852d",
+    "url": "/my-reactapp/static/js/2.a6860f62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-reactapp/static/js/2.a6860f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9255f6eb03de8dcf9069",
+    "url": "/my-reactapp/static/js/main.4c0d8ea2.chunk.js"
+  },
+  {
+    "revision": "e916bc890f51d19a3f2e",
+    "url": "/my-reactapp/static/js/runtime-main.82e8cc0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
